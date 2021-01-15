f3a53c14e86982243a0f976b97f21bb083432059
seok sang hwan <39520391+seoksanghwan@users.noreply.github.com>
2021-01-15 22:55:06 +0900

    Update app.js

--- app.js
+++ app.js
@@ -24,6 +24,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan('dev'));
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use('/', router);
 app.set('json spaces', 2);
 app.set('jwt-secret', config.secret);
